9db1874e868edf7478f375457bc8b642992708f3
hamza <traorehamza407@gmail.com>
2025-07-11 00:24:10 +0200

    Ajout de la gestion des dépendances dans CategoryFixtures et amélioration de la création de catégories avec des associations aléatoires à MangaAnime.

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Category;
+use App\Entity\MangaAnime;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class CategoryFixtures extends Fixture
+class CategoryFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $mangaAnimes = [];
+        for ($i = 0; $i < 10; $i++) {
+            $mangaAnimes[] = $this->getReference('MANGA_ANIME_' . $i, MangaAnime::class);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $category = new Category();
+            $category
+                ->setName($faker->word())
+                ->addMangaAnime($faker->randomElement($mangaAnimes)) // Associe une mangaAnime aléatoire
+            ;
+            $manager->persist($category);
+            $this->addReference('CATEGORY_' . $i, $category);
+        }
 
         $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return [
+            MangaAnimeFixtures::class,
+        ];
     }
 }
