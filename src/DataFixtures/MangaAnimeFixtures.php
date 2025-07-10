847d8b6f7617e9b5deb368679529db4325624504
hamza <traorehamza407@gmail.com>
2025-07-11 00:24:14 +0200

    Amélioration de la génération de fixtures pour MangaAnime avec Faker et ajout de données aléatoires

--- src/DataFixtures/MangaAnimeFixtures.php
+++ src/DataFixtures/MangaAnimeFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\MangaAnime;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -10,11 +11,24 @@ class MangaAnimeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $MangaAnime = new MangaAnime();
-        $MangaAnime
-            ->setTitle('Attack on Titan')
-            ->setType('Anime');
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 10; $i++) {
+            $MangaAnime = new MangaAnime();
+            $MangaAnime
+                ->setTitle($faker->sentence(3))
+                ->setType($faker->randomElement(['Manga', 'Anime']))
+                ->setReleaseDate($faker->dateTimeBetween('-10 years', 'now'))
+                ->setPopularity($faker->numberBetween(1, 100))
+                ->setSynopsis($faker->paragraph(3))
+                ->setAuthor($faker->name())
+                ->setStudio($faker->company())
+                ->setCoverImage($faker->imageUrl(640, 480, 'anime', true, 'Faker'))
+                ->setNumberOfVolumes($faker->numberBetween(1, 50))
+                ->setNumberOfEpisodes($faker->numberBetween(1, 100))
+            ;
+            $manager->persist($MangaAnime);
+            $this->addReference('MANGA_ANIME_' . $i, $MangaAnime);
+        };
 
         $manager->flush();
     }
