8e67f1641ce632178b3a86c1e9039044cdee76b5
hamza <traorehamza407@gmail.com>
2025-07-11 00:25:20 +0200

    Amélioration de RecommendationFixtures avec ajout de la gestion des dépendances et génération de recommandations aléatoires

--- src/DataFixtures/RecommendationFixtures.php
+++ src/DataFixtures/RecommendationFixtures.php
@@ -2,16 +2,44 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\MangaAnime;
+use App\Entity\Recommendation;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class RecommendationFixtures extends Fixture
+class RecommendationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $faker = Factory::create('fr_FR');
+        $mangaAnime = [];
+        for ($i = 0; $i < 10; $i++) {
+            $mangaAnime[] = $this->getReference('MANGA_ANIME_' . $i, MangaAnime::class);
+        }
+        //  Recuperation des utilisateur nouvellement crées
+        $users = [];
+        for ($i = 0; $i < 10; $i++) {
+            $users[] = $this->getReference('USER_' . $i, User::class);
+        }
+        for ($i = 0; $i < 10; $i++) {   
+        $product = new Recommendation();
+        $product
+            ->setScore($faker->numberBetween(1, 10))
+            ->setUsers($faker->randomElement($users)) // Associe un utilisateur aléatoire
+            ->setMangaAnimes($faker->randomElement($mangaAnime)) // Associe une mangaAnime aléatoire
+        ;
+        $manager->persist($product);
+        }
         $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return [
+            MangaAnimeFixtures::class,
+            UserFixtures::class,
+        ];
     }
 }
