e6ae5c353c4cb47729a9559dd086819b6fc9fdf5
hamza <traorehamza407@gmail.com>
2025-07-11 00:24:05 +0200

    Renommage de la classe FavoriFixtures en FavoriteFixtures et amélioration de la gestion des dépendances pour les fixtures d'entités MangaAnime et User.

--- src/DataFixtures/FavoriFixtures.php
+++ src/DataFixtures/FavoriFixtures.php
@@ -2,16 +2,49 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\MangaAnime;
+use App\Entity\User;
+use App\Entity\Favori;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class FavoriFixtures extends Fixture
+class FavoriteFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        //  Recuperation des mangaAnime nouvellement crées
+        $mangaAnime = [];
+        for ($i = 0; $i < 10; $i++) {
+            $mangaAnime[] = $this->getReference('MANGA_ANIME_' . $i, MangaAnime::class);
+        }
+         //  Recuperation des utilisateur nouvellement crées
+        $users = [];
+        for($i = 0; $i < 10; $i++) {
+            $users[] = $this->getReference('USER_' . $i, User::class);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+        $favori = new Favori();
+        $favori
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setMangaAnimes($faker->randomElement($mangaAnime)) // Associe une mangaAnime aléatoire
+            ->setUsers($faker->randomElement($users)) // Associe un utilisateur aléatoire
+            
+        ;
+        $manager->persist($favori);
+        }
 
         $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return [
+            MangaAnimeFixtures::class,
+            UserFixtures::class,
+        ];
     }
 }
