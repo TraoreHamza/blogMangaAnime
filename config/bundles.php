2e5421ee4bf6396dd9a3d297331a18ffdb65e820
hamza <traorehamza407@gmail.com>
2025-08-03 18:15:14 +0200

    Ajout des bundles Dropzone, KnpPaginator, KnpTime et LiveComponent dans la configuration des bundles.

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,8 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
