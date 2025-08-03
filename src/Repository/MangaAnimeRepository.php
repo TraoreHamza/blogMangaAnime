ad8cc354ed2ac95038e25010cb50bdf8bc0499b1
hamza <traorehamza407@gmail.com>
2025-08-03 18:10:06 +0200

    Ajout de la méthode findByFilters dans MangaAnimeRepository pour permettre le filtrage des Manga/Animes par nom, genre et année. Mise à jour de la méthode index dans MangaAnimeController pour passer la liste des Manga/Animes à la vue.

--- src/Repository/MangaAnimeRepository.php
+++ src/Repository/MangaAnimeRepository.php
@@ -16,28 +16,49 @@ class MangaAnimeRepository extends ServiceEntityRepository
         parent::__construct($registry, MangaAnime::class);
     }
 
-//    /**
-//     * @return MangaAnime[] Returns an array of MangaAnime objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return MangaAnime[] Returns an array of MangaAnime objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('m.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?MangaAnime
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?MangaAnime
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+
+    // App\Repository\MangaAnimeRepository.php
+
+    public function findByFilters(array $filters): array
+    {
+        $qb = $this->createQueryBuilder('m');
+
+        if (!empty($filters['name'])) {
+            $qb->andWhere('m.name LIKE :name')
+                ->setParameter('name', '%' . $filters['name'] . '%');
+        }
+        if (!empty($filters['genre'])) {
+            $qb->andWhere('m.genre LIKE :genre')
+                ->setParameter('genre', '%' . $filters['genre'] . '%');
+        }
+        if (!empty($filters['year'])) {
+            $qb->andWhere('m.year = :year')
+                ->setParameter('year', $filters['year']);
+        }
+        return $qb->getQuery()->getResult();
+    }
 }
