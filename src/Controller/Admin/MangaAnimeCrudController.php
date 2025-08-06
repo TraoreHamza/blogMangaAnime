2a5c2fb0d29754d41ace61420d60ba1d1b1bffcd
hamza <traorehamza407@gmail.com>
2025-08-06 16:05:58 +0200

    Refactorisation du contrôleur MangaAnimeCrud : réorganisation des imports, ajout de nouveaux champs pour la configuration des entités et mise à jour des options d'upload d'images.

--- src/Controller/Admin/MangaAnimeCrudController.php
+++ src/Controller/Admin/MangaAnimeCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\MangaAnime;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class MangaAnimeCrudController extends AbstractCrudController
 {
@@ -15,14 +18,42 @@ class MangaAnimeCrudController extends AbstractCrudController
         return MangaAnime::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('type')
+                ->setRequired(true)
+                ->setHelp('Type of the manga or anime, e.g., "Manga", "Anime", etc.'),
+            DateField::new('releaseDate')
+                ->setRequired(true)
+                ->setHelp('Release date of the manga or anime in YYYY-MM-DD format.'),
+            TextEditorField::new('synopsis')
+                ->setRequired(true)
+                ->setHelp('A brief synopsis of the manga or anime.'),
+            IntegerField::new('popularity')
+                ->setRequired(true)
+                ->setHelp('Popularity rating of the manga or anime, e.g., from 1 to 100.'),
+            TextField::new('author')
+                ->setRequired(true)
+                ->setHelp('Name of the author or creator of the manga or anime.'),
+            TextField::new('studio')
+                ->setRequired(true)
+                ->setHelp('Studio that produced the anime, if applicable.'),
+            TextField::new('genre')
+                ->setRequired(true)
+                ->setHelp('Genre of the manga or anime, e.g., "Action", "Romance", etc.'),
+            IntegerField::new('numberOfVolumes')
+                ->setRequired(true)
+                ->setHelp('Current status of the manga or anime, e.g., "Ongoing", "Completed".'),
+            IntegerField::new('numberOfEpisodes')
+                ->setRequired(true)
+                ->setHelp('Current status of the manga or anime, e.g., "Ongoing ", "Completed".'),
+            ImageField::new('image')
+                ->setBasePath('medias/images/')
+                ->setUploadDir('public/medias/images/')
+                ->setUploadedFileNamePattern('[year]/[month]/[day]/[slug]-[contenthash].[extension]'),
         ];
     }
-    */
 }
